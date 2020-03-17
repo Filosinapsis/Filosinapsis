0e2e01783006568b3a5e1f640960515a789e9ae0
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-03-17 17:37:22 -0500

    Add prototype automatization formula
    Added the formula for the automation of neuron position through a
    prototype, to improve positioning function.

---
+++ assets/js/sources.js
@@ -0,0 +1,44 @@
+const Data = [
+    {
+        id: 0,
+        name: 'Matemática',
+        SubNeurons: [1, 2],
+        SuperNeuron: -1
+    },
+    {
+        id: 1,
+        name: 'Matemática Pura',
+        SubNeurons: [2, 3, 4, 5],
+        SuperNeuron: 0
+    },
+    {
+        id: 2,
+        name: 'Números',
+        SubNeurons: [],
+        SuperNeuron: 1
+    },
+    {
+        id: 3,
+        name: 'Geometría',
+        SubNeurons: [],
+        SuperNeuron: 1
+    },
+    {
+        id: 4,
+        name: 'Análisis matemático',
+        SubNeurons: [],
+        SuperNeuron: 1
+    },
+    {
+        id: 5,
+        name: 'Álgebra',
+        SubNeurons: [],
+        SuperNeuron: 1
+    },
+    {
+        id: 6,
+        name: 'Matemática Aplicada',
+        SubNeurons: [3, 4, 5, 6],
+        SuperNeuron: 0
+    }
+];
