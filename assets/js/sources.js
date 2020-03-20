65049998eb2726d8fc39ec6b5ecd3ac2acbb4f62
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-03-19 21:26:57 -0500

    Transfer data on neuron subjects

--- assets/js/sources.js
+++ assets/js/sources.js
@@ -3,42 +3,56 @@ const Data = [
         id: 0,
         name: 'Matemática',
         SubNeurons: [1, 2],
-        SuperNeuron: -1
+        SuperNeuron: -1,
+        color: 'white',
+        size: 'big'
     },
     {
         id: 1,
         name: 'Matemática Pura',
         SubNeurons: [2, 3, 4, 5],
-        SuperNeuron: 0
+        SuperNeuron: 0,
+        color: 'white',
+        size: 'medium'
     },
     {
         id: 2,
         name: 'Números',
         SubNeurons: [],
-        SuperNeuron: 1
+        SuperNeuron: 1,
+        color: 'yellow',
+        size: 'small'
     },
     {
         id: 3,
         name: 'Geometría',
         SubNeurons: [],
-        SuperNeuron: 1
+        SuperNeuron: 1,
+        color: 'green',
+        size: 'small'
     },
     {
         id: 4,
         name: 'Análisis matemático',
         SubNeurons: [],
-        SuperNeuron: 1
+        SuperNeuron: 1,
+        color: 'red',
+        size: 'small'
     },
     {
         id: 5,
         name: 'Álgebra',
         SubNeurons: [],
-        SuperNeuron: 1
+        SuperNeuron: 1,
+        color: 'skyblue',
+        size: 'small'
     },
     {
         id: 6,
         name: 'Matemática Aplicada',
         SubNeurons: [3, 4, 5, 6],
-        SuperNeuron: 0
+        SuperNeuron: 0,
+        color: 'white',
+        size: 'medium'
     }
 ];
